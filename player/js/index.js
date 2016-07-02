6da0f7eccb34015faa83aff12a176e764b994f41
Zhou Chang <achang.zhou@qq.com>
2016-07-02 14:14:52 +0800

    Just backup
    Signed-off-by: Zhou Chang <achang.zhou@qq.com>

--- player/js/index.js
+++ player/js/index.js
@@ -1,5 +1,27 @@
 'use strict';
 
+function buildClickFunction(row, col) {
+  return function() {
+    console.log(">>>>:" + row + " " + col);
+  };
+}
+
 $(document).ready(function () {
+  // 初始化棋盘组件
+  for (var i = 0; i <= 16; i++) {
+    for (var j = 0; j <= 16; j++) {
+      var name = "P_" + i + "_" + j
+      var item = '<li id="' + name + '" ></li>'
+      $("#L" + i ).append(item);
+      $("#" + name).click( buildClickFunction(i,j));
+    }
+  }
+
+  // 设置棋子
+  var item = $("#P_10_10")
+  $('<img>').attr('src', 'images/b0.png').attr("class", "chess").appendTo(item);
+
+  var item = $("#P_4_9")
+  $('<img>').attr('src', 'images/r9.png').attr("class", "chess").appendTo(item);
 
 });
